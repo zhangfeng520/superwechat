af315d510467722a78573d92276b5ba81e447379
zhangfeng <418263192@163.com>
2016-07-25 11:21:31 +0800

    闪屏界面的优化

--- src/cn/ucai/superwechat/activity/SplashActivity.java
+++ src/cn/ucai/superwechat/activity/SplashActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,11 +14,16 @@ import android.widget.TextView;
 import com.easemob.chat.EMChatManager;
 import com.easemob.chat.EMGroupManager;
 import cn.ucai.superwechat.DemoHXSDKHelper;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.Utils;
+import cn.ucai.superwechat.bean.Result;
 import cn.ucai.superwechat.bean.UserAvatar;
+import cn.ucai.superwechat.data.OkHttpUtils2;
 import cn.ucai.superwechat.db.UserDao;
 import cn.ucai.superwechat.task.DownloadContactListTask;
+import cn.ucai.superwechat.utils.UserUtils;
 
 /**
  * 开屏页
@@ -62,8 +68,34 @@ public class SplashActivity extends BaseActivity {
 					UserDao dao = new UserDao(SplashActivity.this);
 					UserAvatar user = dao.getUserAvatar(userName);
 					Log.e(TAG, "user=" + user);
-					if (user != null) {
+					//闪屏优化
+					if (user == null) {final OkHttpUtils2<String> utils = new OkHttpUtils2<String>();
+						utils.setRequestUrl(I.REQUEST_FIND_USER)
+								.addParam(I.User.USER_NAME,userName)
+								.targetClass(String.class)
+								.execute(new OkHttpUtils2.OnCompleteListener<String>() {
+									@Override
+									public void onSuccess(String s) {
+										Log.e(TAG, "s" + s);
+										Result result = Utils.getResultFromJson(s, UserAvatar.class);
+										if (result != null && result.isRetMsg()) {
+											UserAvatar user = (UserAvatar) result.getRetData();
+											Log.e(TAG, "user=" + user);
+											if (user != null) {
+												SuperWeChatApplication.getInstance().setUser(user);
+												SuperWeChatApplication.currentUserNick = user.getMUserNick();
+											}
+										}
+									}
 
+									@Override
+									public void onError(String error) {
+										Log.e(TAG, "error+" + error);
+									}
+								});
+
+					}
+					else if(user != null){
 						SuperWeChatApplication.getInstance().setUser(user);
 						SuperWeChatApplication.currentUserNick = user.getMUserNick();
 					}
